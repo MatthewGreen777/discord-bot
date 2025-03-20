19be2d486bffaee9bcf3ad8241dd2d5a306fb58f
MatthewGreen777 <mgreen11@umbc.edu>
2025-03-20 16:00:29 -0400

    added command to github

---
+++ commands/utility/info-github.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('infogithub')
+		.setDescription('See source code for the bot.'),
+	async execute(interaction) {
+        await interaction.reply(`This is the GitHub link of the bot: https://github.com/MatthewGreen777/discord-bot`);
+	},
+};
