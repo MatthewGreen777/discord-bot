c938e5a065b0e17cabeef375b3194cab3a384abc
MatthewGreen777 <mgreen11@umbc.edu>
2025-03-20 16:04:33 -0400

    fixed spacing

--- commands/utility/info-github.js
+++ commands/utility/info-github.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('infogithub')
+		.setName('info-github')
 		.setDescription('See source code for the bot.'),
 	async execute(interaction) {
         await interaction.reply(`This is the GitHub link of the bot:\nhttps://github.com/MatthewGreen777/discord-bot`);
