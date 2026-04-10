92304167eced47a77e1a2779366a101b8354a331
MatthewGreen777 <mgreen11@umbc.edu>
2026-04-10 14:22:19 -0400

    update to server info

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,10 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Provides information about the server.'),
-	async execute(interaction) {
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
-	},
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Provides information about the server.'),
+    async execute(interaction) {
+        const { guild } = interaction;
+
+        // Discord Timestamps: 
+        // <t:SECONDS:F> shows a full date/time (e.g., April 10, 2026 5:00 PM)
+        // <t:SECONDS:R> shows a relative time (e.g., 2 years ago)
+        const createdTimestamp = Math.floor(guild.createdTimestamp / 1000);
+
+        const serverEmbed = new EmbedBuilder()
+            .setTitle(guild.name)
+            .setThumbnail(guild.iconURL())
+            .setColor(0x5865F2)
+            .addFields(
+                { name: 'Member Count', value: `${guild.memberCount}`, inline: true },
+                { name: 'Created On', value: `<t:${createdTimestamp}:F>\n(<t:${createdTimestamp}:R>)`, inline: true }
+            );
+
+        await interaction.reply({ embeds: [serverEmbed] });
+    },
 };
